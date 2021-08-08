dd4155f6908a53658b3d817ef6da7e12a5b450a5
semih <semih.ucak@student.odisee.be>
2021-08-08 22:27:48 +0200

    big update placing 3d objects

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 import * as THREE from '../build/three.module.js';
 import { ARButton } from './jsm/webxr/ARButton.js';
 
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from './jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from './jsm/utils/RoughnessMipmapper.js';
+
 let container;
 let camera, scene, renderer;
 let controller;
 
-let reticle;
+let reticle, pmremGenerator, current_object, controls;
 
 let hitTestSource = null;
 let hitTestSourceRequested = false;
@@ -13,16 +18,59 @@ let hitTestSourceRequested = false;
 init();
 animate();
 
+$(".ar-object").click(function () {
+	if (current_object != null) {
+		scene.remove(current_object);
+	}
+
+	loadModel($(this).attr("id"));
+});
+
+$("#ARButton").click(function () {
+	current_object.visible = false;
+	isAR = true;
+});
+
+function loadModel(model) {
+	new RGBELoader()
+		.setDataType(THREE.UnsignedByteType)
+		.setPath('textures/')
+		.load('photo_studio_01_1k.hdr', function (texture) {
+
+			var envmap = pmremGenerator.fromEquirectangular(texture).texture;
+
+			scene.environment = envmap;
+			texture.dispose();
+			pmremGenerator.dispose();
+			render();
+
+			var loader = new GLTFLoader().setPath('models/');
+			loader.load(model + ".gltf", function (glb) {
+
+				current_object = glb.scene;
+				scene.add(current_object);
+
+				//center the object in the screen
+				var box = new THREE.Box3();
+				box.setFromObject(current_object);
+				box.center(controls.target);
+
+				controls.update();
+				render();
+			});
+		});
+}
+
 function init() {
 
 	container = document.createElement('div');
-	document.body.appendChild(container);
+	document.getElementById("container").appendChild(container);
 
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.001, 200);
 
-	const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 	light.position.set(0.5, 1, 0.25);
 	scene.add(light);
 
@@ -33,6 +81,17 @@ function init() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.xr.enabled = true;
 	container.appendChild(renderer.domElement);
+
+	pmremGenerator = new THREE.PMREMGenerator(renderer);
+	pmremGenerator.compileEquirectangularShader();
+
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.addEventListener('change', render);
+	controls.minDistance = 2;
+	controls.maxDistance = 10;
+	controls.target.set(0, 0, -0.2);
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.05; // smooth the rotation
 
 	//
 
@@ -46,11 +105,14 @@ function init() {
 
 		if (reticle.visible) {
 
-			const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
+			/*const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
 			const mesh = new THREE.Mesh(geometry, material);
 			mesh.position.setFromMatrixPosition(reticle.matrix);
 			mesh.scale.y = Math.random() * 2 + 1;
-			scene.add(mesh);
+			scene.add(mesh);*/
+
+			current_object.position.setFromMatrixPosition(reticle.matrix);
+			current_object.visible = true;
 
 		}
 
@@ -88,6 +150,8 @@ function onWindowResize() {
 function animate() {
 
 	renderer.setAnimationLoop(render);
+	requestAnimationFrame(animate);
+	controls.update();
 
 }
 
@@ -114,6 +178,12 @@ function render(timestamp, frame) {
 
 				hitTestSourceRequested = false;
 				hitTestSource = null;
+
+				reticle.visible = false;
+
+				var box = new THREE.Box3();
+				box.setFromObject(current_object);
+				box.center(controls.target);
 
 			});
 
