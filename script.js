577e446c041dead1b5fe436337f4dcc83c83fd0a
semih <semih.ucak@student.odisee.be>
2021-08-11 22:24:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-import * as THREE from '../build/three.module.js';
+import * as THREE from './build/three.module.js';
 import { ARButton } from './jsm/webxr/ARButton.js';
 
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
